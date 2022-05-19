d87bf323f3680cf471718e1fabc949a1066378a2
ABEL003 <sim.feldmann24@gmail.com>
2022-05-19 18:34:05 +0200

    added new matrix code

---
+++ Pxie matrix code v2/Pxie_matrix_code_v2.ino/Pxie_matrix_code_v2.ino.ino
@@ -0,0 +1,75 @@
+#include <FastLED.h>
+
+#define NUM_LEDS 8
+#define DATA_PIN 11
+
+CRGB leds[NUM_LEDS];
+
+//Zeile mit Input Pins
+byte rows[] = {2, 3, 4, 5};
+const int rowCount = sizeof(rows) / sizeof(rows[0]); //rechnet die Länge der Liste aus
+
+//Spalte mit Output Pins
+byte cols[] = {8, 9, 10};
+const int colCount = sizeof(cols) / sizeof(cols[0]); //rechnet die Länge der Liste aus
+
+byte keys[colCount][rowCount]; //Liste der Tastenzustände
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  
+  Serial.begin(9600);
+
+  for (int x = 0; x < rowCount; x++) {
+    pinMode(rows[x], INPUT);
+  }
+
+  for (int x = 0; x < colCount; x++) {
+    pinMode(cols[x], INPUT_PULLUP);
+  }
+}
+
+void readMatrix() {
+  // Prüft die Spalten (col)
+  for (int colIndex = 0; colIndex < colCount; colIndex++) {
+    // col auf output low
+    byte curCol = cols[colIndex];
+    pinMode(curCol, OUTPUT);
+    digitalWrite(curCol, LOW);
+
+   // Prüft die Zeilen (row) der jeweiligen Spalte (col)
+    for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+      byte rowCol = rows[rowIndex];
+      pinMode(rowCol, INPUT_PULLUP);
+      keys[colIndex][rowIndex] = digitalRead(rowCol);
+      pinMode(rowCol, INPUT);
+    }
+    // schaltet die Spalte wieder aus
+    pinMode(curCol, INPUT);
+  }
+}
+
+void printMatrix() {
+
+  for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+
+    Serial.print(F("R_"));
+    Serial.print(rowIndex + 1); Serial.print(F(": ")); Serial.print("\t");
+
+    for (int colIndex = 0; colIndex < colCount; colIndex++) {
+      Serial.print(keys[colIndex][rowIndex]);
+      if (colIndex < colCount)
+        Serial.print("\t");
+    }
+    Serial.println("");
+  }
+  Serial.println("");
+}
+
+void loop() {
+  readMatrix();
+
+  printMatrix();
+  delay(1000);
+
+}
