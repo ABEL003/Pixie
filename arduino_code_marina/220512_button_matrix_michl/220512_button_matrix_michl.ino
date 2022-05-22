66a10d5d37d7ed37372c1d378ae8298092db2595
Marina <marina.beck@hfg-gmuend.de>
2022-05-22 15:13:40 +0200

    marinas arduino codes

---
+++ arduino_code_marina/220512_button_matrix_michl/220512_button_matrix_michl.ino
@@ -0,0 +1,66 @@
+//Zeile mit Input Pins
+byte rows[] = {2, 3, 4, 5};
+const int rowCount = sizeof(rows) / sizeof(rows[0]); //rechnet die Länge der Liste aus
+
+//Spalte mit Output Pins
+byte cols[] = {8, 9, 10};
+const int colCount = sizeof(cols) / sizeof(cols[0]); //rechnet die Länge der Liste aus
+
+byte keys[colCount][rowCount]; //Liste der Tastenzustände 
+void setup() {
+  Serial.begin(9600);
+
+  for (int x = 0; x < rowCount; x++) {        //alle rows sind ein INPUT
+    pinMode(rows[x], INPUT);
+  }
+
+  for (int x = 0; x < colCount; x++) {        //alle columns sind ein INPUT
+    pinMode(cols[x], INPUT_PULLUP);
+  }
+
+}
+
+void readMatrix() {
+  // Prüft die Spalten (col)
+  for (int colIndex = 0; colIndex < colCount; colIndex++) {
+    // col auf output low
+    byte curCol = cols[colIndex];
+    pinMode(curCol, OUTPUT);
+    digitalWrite(curCol, LOW);
+
+   // Prüft die Zeilen (row) der jeweiligen Spalte (col)
+    for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+      byte rowCol = rows[rowIndex];
+      pinMode(rowCol, INPUT_PULLUP);
+      keys[colIndex][rowIndex] = digitalRead(rowCol); //
+      pinMode(rowCol, INPUT);
+    }
+    // schaltet die Spalte wieder aus
+    pinMode(curCol, INPUT);
+  }
+}
+
+void printMatrix() {
+
+  for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+
+    Serial.print(F("R_"));
+    Serial.print(rowIndex + 1); Serial.print(F(": ")); Serial.print("\t");
+
+    for (int colIndex = 0; colIndex < colCount; colIndex++) {
+      Serial.print(keys[colIndex][rowIndex]);
+      if (colIndex < colCount)
+        Serial.print("\t");
+    }
+    Serial.println("");
+  }
+  Serial.println("");
+}
+
+void loop() {
+  readMatrix();
+
+  printMatrix();
+  delay(500);
+
+}
